48b6aa43dce9f1aa2c46d41aed493954b903324b
geodalli <georgiadalli1995@gmail.com>
2018-11-03 01:28:42 +0200

    epitelous to sinxronisa :D
    Co-Authored-By: Loukas Spanidis <loukspan@hotmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main { 
 	
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
 		//We create the players and the board
         //MaxDepth for the MiniMax algorithm is set to 2; feel free to change the values
